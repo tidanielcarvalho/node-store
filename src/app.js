9887fe1a84e98f999caf7dbfbaafa5653ce1d1fd
Daniel <ti.danielcarvalho@gmail.com>
2020-02-17 00:08:07 -0300

    Separado arquivo app de serverjs

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title:"Node Store Api",
-        version: "0.0.1"
-    });
-});
-app.use('/', route);
+var options = { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true };
+mongoose.connect('string de conexao', options);
+
+// Load model
+const Product = require('./models/product');
+
+// Load router
+const indexRoute = require('./routes/indexRouter');
+const productRoute = require('./routes/productRouter');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
-
-
