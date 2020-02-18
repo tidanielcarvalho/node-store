b4a8b938ab02e996029738a86b6dd9684566c909
Daniel <ti.danielcarvalho@gmail.com>
2020-02-17 23:55:39 -0300

    Importado novos models, controllers e routers para app

--- src/app.js
+++ src/app.js
@@ -7,20 +7,25 @@ const mongoose = require('mongoose');
 const app = express();
 const router = express.Router();
 
-var options = { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true };
+var options = { useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true };
 mongoose.connect('string de conexao', options);
 
 // Load model
 const Product = require('./models/product');
+const Customer = require('./models/customer');
+const Order = require('./models/order');
+
 
 // Load router
 const indexRoute = require('./routes/indexRouter');
 const productRoute = require('./routes/productRouter');
+const customerRoute = require('./routes/customerRouter');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
+app.use('/customers', customerRoute);
 
 module.exports = app;
